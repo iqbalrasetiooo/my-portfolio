325fb8c21463db4d1015c0396d7c65bf2c1cb2a9
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-06-23 20:26:01 +0700

    add new file customization.js to customization my own library

---
+++ customization.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            sans: ['Plus Jakarta Sans', 'sans-serif'],
+          },
+    extend: {
+        colors: {
+        clifford: '#da373d',
+        }
+    }
+    }
+}
